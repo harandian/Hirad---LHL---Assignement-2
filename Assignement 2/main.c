6ad708cba9a21b722702aaa3129f20f6801c2f4e
Hirad <hharandian@gmail.com>
2017-07-17 14:19:00 -0700

    LHL prep course

---
+++ Assignement 2/main.c
@@ -0,0 +1,59 @@
+//
+//  main.c
+//  Assignement 2
+//
+//  Created by Hirad on 2017-05-10.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    char play;
+    int choice;
+    
+    printf("Welcome to Two doors.\n");
+    printf("Would you like to play? (y/n): ");
+    scanf("%c", &play);
+    
+    if (play == 'y') {
+        
+        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+        printf("What do you ask so you can pick the door to freedom?\n\n");
+        printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+        printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+        printf("\t3.Doesn't matter which one you pick.\n");
+        
+        
+        printf("What do you do? (1,2 or 3?): \n");
+        scanf("%d", &choice);
+        
+        if (choice == '\0' && choice > 3) {
+            printf("\nWhat do you do? (1,2 or 3?): \n");
+            scanf("%d", &choice);
+        }
+        else
+        {
+        char *answer = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";
+        switch (choice) {
+            case 1:
+                printf("%s", answer);
+                break;
+            case 2:
+                printf("%s", answer);
+                break;
+            case 3:
+                printf("%s", answer);
+                break;
+            default:
+                break;
+        }
+        }
+    }
+    
+    return 0;
+}
